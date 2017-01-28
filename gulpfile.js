f20fe4d064d96158f0fa67e259f78b4e54e72695
Ivan Krivonos <devbackend@yandex.ru>
2017-01-28 19:03:34 +1000

    Инициализация фронтенда

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,6 @@
 const elixir = require('laravel-elixir');
+
+elixir(function(mix) {
+	mix.copy('node_modules/material-design-lite/material.min.css', 'public/css/material.min.css');
+	mix.copy('node_modules/material-design-lite/material.min.js', 'public/js/material.min.js');
+});
