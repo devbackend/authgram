a3d22d7a8829437c6c91cbcaa93529813a83fca3
Ivan Krivonos <devbackend@yandex.ru>
2017-03-05 17:22:29 +1000

    Ишем в лог команду, которая запускается при деплое

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -32,22 +32,27 @@ while(time() < PHP_INT_MAX) {
 		chdir(__DIR__ . '/..');
 
 		writeToLog($deployHandler, 'Выполнение git pull');
+		writeToLog($deployHandler, '/usr/bin/git pull >> ' . $releaseDir . '/git.log');
 		shell_exec('/usr/bin/git pull >> ' . $releaseDir . '/git.log');
 		writeToLog($deployHandler, 'Выполнен git pull');
 
 		writeToLog($deployHandler, 'Установка зависимостей Composer');
+		writeToLog($deployHandler, '/usr/bin/composer install >> ' . $releaseDir . '/composer.log');
 		shell_exec('/usr/bin/composer install >> ' . $releaseDir . '/composer.log');
 		writeToLog($deployHandler, 'Завершение установки зависимостей Composer');
 
 		writeToLog($deployHandler, 'Установка зависимостей Yarn');
+		writeToLog($deployHandler, '/usr/local/bin/yarn install >> ' . $releaseDir . '/yarn.log');
 		shell_exec('/usr/local/bin/yarn install >> ' . $releaseDir . '/yarn.log');
 		writeToLog($deployHandler, 'Завершение установки зависимостей Yarn');
 
 		writeToLog($deployHandler, 'Выполнение миграций');
+		writeToLog($deployHandler, '/usr/bin/php ' . __DIR__ . '/../artisan migrate --force >> ' . $releaseDir . '/migrate.log');
 		shell_exec('/usr/bin/php ' . __DIR__ . '/../artisan migrate --force >> ' . $releaseDir . '/migrate.log');
 		writeToLog($deployHandler, 'Завершение выполнения миграций');
 
 		writeToLog($deployHandler, 'Сборка фронтенда');
+		writeToLog($deployHandler, '/usr/local/bin/gulp --production >> ' . $releaseDir . '/gulp.log');
 		shell_exec('/usr/local/bin/gulp --production >> ' . $releaseDir . '/gulp.log');
 		writeToLog($deployHandler, 'Завершение сборки фронтенда');
 
