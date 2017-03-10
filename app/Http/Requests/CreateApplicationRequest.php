4074b2383ea4a9501cec34bbd233b3c618633f09
Ivan Krivonos <devbackend@yandex.ru>
2017-03-10 22:42:54 +1000

    Добавлен phpdoc для запроса создания приложения

--- app/Http/Requests/CreateApplicationRequest.php
+++ app/Http/Requests/CreateApplicationRequest.php
@@ -5,6 +5,11 @@ namespace App\Http\Requests;
 use App\Entities\Application;
 use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * Данные формы создания нового приложения
+ *
+ * @author Кривонос Иван <devbackend@yandex.ru>
+ */
 class CreateApplicationRequest extends FormRequest {
 	/**
 	 * Имеет ли текущий пользователь право на отправку запроса.
@@ -27,7 +32,7 @@ class CreateApplicationRequest extends FormRequest {
 	public function rules() {
 		return [
 			Application::TITLE              => 'required|min:5',
-			Application::WEBSITE            => 'required|url|unique:applications',
+			Application::WEBSITE            => 'required|url|unique:' . Application::table(),
 			Application::AUTH_REQUEST_URL   => 'required|url',
 		];
 	}
