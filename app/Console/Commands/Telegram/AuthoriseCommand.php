095b62215b131dca7473ab1f7c604a331ae09d8d
Ivan Krivonos <devbackend@yandex.ru>
2017-08-17 19:48:01 +1000

    Исправления интерфейса для пользователей

--- app/Console/Commands/Telegram/AuthoriseCommand.php
+++ app/Console/Commands/Telegram/AuthoriseCommand.php
@@ -138,7 +138,7 @@ class AuthoriseCommand extends TelegramCommand {
 		$authAttempt->save();
 		//-- -- -- --
 
-		$replyMessage->setText('Вы успешно авторизовались');
+		$replyMessage->setText("Вы успешно авторизовались. \n\n Вернитесь в браузер, чтобы продолжить работу с сайтом.");
 
 		$this->replyWithMessage($replyMessage->get());
 	}
