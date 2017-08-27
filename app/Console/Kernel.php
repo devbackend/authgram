99730be08b69415e475f2a4cda208f1f5274f1b5
Ivan Krivonos <devbackend@yandex.ru>
2017-08-27 17:37:59 +1000

    Добавлено расписание для удаления устаревших логов

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,10 @@ namespace App\Console;
 
 use App\Console\Commands\CreateTelegramCommand;
 use App\Console\Commands\WebhookSwitchCommand;
+use App\Entities\LogIncomeMessage;
+use Exception;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Database\Connection;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 /**
@@ -26,5 +29,23 @@ class Kernel extends ConsoleKernel {
 	 *
 	 * @author Кривонос Иван <devbackend@yandex.ru>
 	 */
-	protected function schedule(Schedule $schedule) {}
+	protected function schedule(Schedule $schedule) {
+		$schedule->call(function() {
+			$minimalLogDate = date('c', time() - 50 * 86400);
+
+			/** @var Connection $db */
+			$db = resolve(Connection::class);
+
+			$db->beginTransaction();
+
+			try {
+				$db->table('log_income_messages')->where(LogIncomeMessage::CREATED_AT, '<', $minimalLogDate)->delete();
+				$db->table('log_auth_attempts')->where(LogIncomeMessage::CREATED_AT, '<', $minimalLogDate)->delete();
+
+				$db->commit();
+			} catch (Exception $e) {
+				$db->rollBack();
+			}
+		})->daily();
+	}
 }
