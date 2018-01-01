44276d595a4443b9e3de6a6b58541f840adc38f1
Ivan Krivonos <devbackend@yandex.ru>
2018-01-01 06:33:21 +0000

    website.js edited online with Bitbucket

--- resources/assets/js/website.js
+++ resources/assets/js/website.js
@@ -10,10 +10,10 @@
 		$(".button-collapse").sideNav();
 
 		$('[data-role="mobile-menu"]').sideNav({
-			menuWidth: 300, // Default is 300
-			edge: 'left', // Choose the horizontal origin
-			closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-			draggable: true // Choose whether you can drag to open on touch screens
+			menuWidth: 300,		// Default is 300
+			edge: 'left', 		// Choose the horizontal origin
+			closeOnClick: true,	// Closes side-nav on <a> clicks, useful for Angular/Meteor
+			draggable: true 	// Choose whether you can drag to open on touch screens
 		});
 
 		$('[data-activates="profile-menu"]').dropdown({
